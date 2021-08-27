e870e38105433b2af369b3a389df2e7e8c7d1d0c
Alex <alexespinoza11f@gmail.com>
2021-08-26 22:03:33 -0700

    Added function to intialize an array of users/password/ids for app.

--- app/src/main/java/com/example/basiclogin/LoginActivity.java
+++ app/src/main/java/com/example/basiclogin/LoginActivity.java
@@ -7,25 +7,61 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText username;
-    private EditText password;
-    private Button login;
+    private EditText et_username;
+    private EditText et_password;
+    private Button btn_login;
+
+    private List<Map<String, String>> users =  new ArrayList<Map<String, String>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = findViewById(R.id.et_username);
-        password = findViewById(R.id.et_password);
-        login = findViewById(R.id.btn_login);
-        login.setOnClickListener(new View.OnClickListener() {
+        intializeUsers(users);
+//        print(users);
+
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("working");
+                String username = et_username.getText().toString();
+                String password = et_password.getText().toString();
+
+
             }
         });
     }
+
+//    public void isValidCredentials;
+
+    public void intializeUsers(List<Map<String, String>> users){
+        String[] usernames = {"alex", "andy", "michael", "eric", "prince", "aundre", "steven", "edward", "nick", "chris"};
+        String[] passwords = {"123", "espi", "hay", "chav", "ri", "lab", "sch", "clus", "floor", "her"};
+
+        for(int i = 1; i <= 10; i++){
+            Map<String, String> obj = new HashMap<String, String>();
+            obj.put("username", usernames[i-1]);
+            obj.put("password", passwords[i-1]);
+            obj.put("id", Integer.toString(i));
+            users.add(obj);
+        }
+    }
+
+//    public void print(List<Map<String, String>> users){
+//        for(Map m : users){
+//            System.out.println(m.get("username"));
+//            System.out.println(m.get("password"));
+//            System.out.println(m.get("id"));
+//        }
+//    }
 }
